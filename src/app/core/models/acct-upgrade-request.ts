a6529398ab69fd6aaf1df205b746afb079e8051d
Benjamin Oriaifo <Benjamin.Oriaifo@stanbicibtc.com>
2020-10-20 09:52:20 +0100

    first commit

---
+++ src/app/core/models/acct-upgrade-request.ts
@@ -0,0 +1,19 @@
+export class AccountUpgradeRequest{
+    accountNumber: string;
+    accountName: string;
+    phoneNumber: string;
+    oldScheme: string;
+    newScheme: string;
+    newAddress: string;
+    idType: string;
+    idNumber: string;
+    idDoc: string;
+    idDocExtension: string;
+    picDoc: string;
+    picDocExtension: string;
+    utilityDoc: string;
+    utilityDocExtension: string;
+    haveDebitCard: string;
+    signatureDoc: string;
+    signatureDocExtension: string;
+  }
---
+++ src/app/pages/account-reactivation/account-reactivation.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/app/shared/material.module';
+import { Routes, RouterModule } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { AccountReactivationComponent } from './account-reactivation.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AccountReactivationComponent,
+  },
+];
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(routes),
+    MatInputModule,
+    MaterialModule,
+    MatFormFieldModule,
+  ],
+  exports: [],
+  declarations: [AccountReactivationComponent],
+  providers: [],
+})
+export class AccountReactivationModule {}
