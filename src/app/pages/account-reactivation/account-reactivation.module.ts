3066385b4e4e03601c2c209103b675384c267cce
Benjamin Oriaifo <Benjamin.Oriaifo@stanbicibtc.com>
2020-10-26 11:37:56 +0100

    new merge from quality-assurance

--- src/app/pages/account-reactivation/account-reactivation.module.ts
+++ src/app/pages/account-reactivation/account-reactivation.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     MatFormFieldModule,
   ],
   exports: [],
-  declarations: [],
+  declarations: [AccountReactivationComponent],
   providers: [],
 })
 export class AccountReactivationModule {}
